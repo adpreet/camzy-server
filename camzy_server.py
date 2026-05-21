d4131055d1b86ab91cb67cb6e8da735827d084ad
adpreet <39778544+adpreet@users.noreply.github.com>
2026-05-21 17:01:22 -0400

    Update camzy_server.py

--- camzy_server.py
+++ camzy_server.py
@@ -16,6 +16,7 @@ class Handler(BaseHTTPRequestHandler):
                     "-g",
                     "--format", "best[ext=mp4]/best",
                     "--extractor-args", f"youtube:player_client={client}",
+"--cookies", "cookies.txt",
                     "--no-check-certificate",
                     "https://www.youtube.com/watch?v=" + video_id
                 ],
